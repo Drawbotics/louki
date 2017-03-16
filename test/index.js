17f2ad0df0c7ff285cf4842c2333a51c9f1a2b23
nicmosc <nmoscholios@gmail.com>
2017-03-16 12:10:00 +0100

    create basic structure

---
+++ test/index.js
@@ -0,0 +1,3 @@
+import 'babel-polyfill';
+import './unit';
+import './integration';
---
+++ test/integration/awesome.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai';
+
+
+describe('AWESOME EXAMPLE', function() {
+  before(function() {
+  });
+
+  describe('after loading source files', function() {
+    it('not sure yet', function() {
+
+    });
+  });
+});
