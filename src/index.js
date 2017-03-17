79816e0a1917efcee1130ff70c309c0f414db78c
nicmosc <nmoscholios@gmail.com>
2017-03-17 11:44:32 +0100

    read all leaves properly

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import { find, isEmpty } from 'lodash';
 import getSectionsTree from './get-sections-tree';
 
 
-function parseTree(tree, rootFolder) {
+function parseTree(rootFolder) {
+  const tree = getSectionsTree(rootFolder);
   const { children } = tree;
 
   const manifestFile = find(children, { extension: 'json' });
+  let final = '';
 
   if (manifestFile) {
     const manifest = JSON.parse(manifestFile.content);
@@ -19,27 +21,54 @@ function parseTree(tree, rootFolder) {
         if ( ! isEmpty(matchExample)) {
           const folder = matchExample[1];
           console.log('FOLDER -- ', folder);
-          awesome(`${rootFolder}/${folder}`);
+          final += `${key}: \n\t${parseTree(`${rootFolder}/${folder}`)}`;
+          // console.log('temp+++',temp, '+++');
+        }
+
+        else {
+          const simpleKey = `${key}: ${val}`;
+          console.log('Simple key: ', simpleKey);
+
+          final += simpleKey;
         }
       }
+      console.log('manifest has no property');
+      // return '';
     }
   }
   else {
-    console.log('no manifest');  // here we read the yml
+    console.log('no manifest, gonna read yml');
     const index = find(children, { extension: 'yml' });
 
     if (index) {
       const translations = index.content;
       console.log(translations);
+      final += translations;
     }
+    console.log('no yml');
+    // return '';
     // console.log(index);
   }
 
+  return final;
 }
 
-export default function awesome(rootFolder, targetFolder='') {
-  const tree = getSectionsTree(rootFolder);
-  console.log('-------------------');
-  parseTree(tree, rootFolder);
 
+// function parseTree(rootFolder) {
+//   /*
+//     if folder has manifest, iterate over folders && items
+//       if folder => return fullTran + parseTree(rootFolder)
+//       else return simpleKey
+//     if folder does not have manifest, return translation
+//   */
+//
+//
+// }
+
+export default function awesome(rootFolder, targetFolder='') {
+  // const fullFile = readSections(rootFolder);
+  // const tree = getSectionsTree(rootFolder);
+  const fullFile = parseTree(rootFolder);
+  console.log('==============================');
+  console.log(fullFile);
 }
