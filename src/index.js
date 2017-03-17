1cfdb3e2c95d945c2e6213b804174ebef203e3c6
nicmosc <nmoscholios@gmail.com>
2017-03-17 09:50:12 +0100

    recursively traverse sections and read contents of yml files

--- src/index.js
+++ src/index.js
@@ -1,21 +1,45 @@
 import path from 'path';
-import { find } from 'lodash';
+import { find, isEmpty } from 'lodash';
 
 import getSectionsTree from './get-sections-tree';
 
 
-function parseTree(tree) {
+function parseTree(tree, rootFolder) {
   const { children } = tree;
 
-  const manifest = JSON.parse(find(children, { extension: 'json' }).content);
-  
+  const manifestFile = find(children, { extension: 'json' });
 
-  console.log(manifest);
+  if (manifestFile) {
+    const manifest = JSON.parse(manifestFile.content);
+    for (const key in manifest) {
+      if (manifest.hasOwnProperty(key)) {
+        const val = manifest[key];
+        const matchExample = val.match(/{{(.*)}}/);
+
+        if ( ! isEmpty(matchExample)) {
+          const folder = matchExample[1];
+          console.log('FOLDER -- ', folder);
+          awesome(`${rootFolder}/${folder}`);
+        }
+      }
+    }
+  }
+  else {
+    console.log('no manifest');  // here we read the yml
+    const index = find(children, { extension: 'yml' });
+
+    if (index) {
+      const translations = index.content;
+      console.log(translations);
+    }
+    // console.log(index);
+  }
+
 }
 
-export default function awesome(rootFolder, targetFolder) {
-
+export default function awesome(rootFolder, targetFolder='') {
   const tree = getSectionsTree(rootFolder);
-  parseTree(tree);
+  console.log('-------------------');
+  parseTree(tree, rootFolder);
 
 }
