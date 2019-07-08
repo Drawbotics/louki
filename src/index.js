e5c413aec9deeff59fd531df92c7270a4100d34c
nicmosc <nmoscholios@gmail.com>
2019-07-08 10:30:41 +0200

    update source

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 import watch from 'node-watch';
 
-import pull from './pull';
-import push from './push';
 import update from './update';
-import init from './init';
+import build from './build';
 
 
 /**
   AVAILABLE COMMANDS:
-  - update : will parse the translations from the root folder provided and replae
+  - build : will parse the translations from the root folder provided and replace
     the contents of dist with the new translation (only the default local will be replaced)
-  - push : will call update and then through localeapp it will push the final yml file (default local only again)
-  - pull : will retrieve the data from localeapp and replace the contents of dist with those files, it will
-    also disect the large translation file (en.yml) and replace the contents of the index files.
+  - update : will disect the large translation file ([default].yml) and replace the contents of the index files
 **/
 
 
-export default function louki(command, rootFolder, targetPath, option, extra={}) {
-  const { pushDefault, watchFiles, raw } = extra;
+export default function louki(command, rootFolder, targetPath, option, extra) {
+  const { watchFiles } = extra;
   const defaultLocale = option;
-  if (command === 'init') {
-    init(option);
-  }
-  else if (command === 'build') {
+  if (command === 'build') {
     if (watchFiles) {
       console.log('Louki watching for changes in root folder...');
       update(rootFolder, targetPath, defaultLocale); // run once
       watch(rootFolder, { recursive: true, filter: /\.(json|yml)$/ }, (evt, fileName) => {
         console.log(evt, fileName.replace(rootFolder, ''));
-        return update(rootFolder, targetPath, defaultLocale);
+        return build(rootFolder, targetPath, defaultLocale);
       });
     }
     else {
-      return update(rootFolder, targetPath, defaultLocale);
+      return build(rootFolder, targetPath, defaultLocale);
     }
   }
   else if (command === 'update') {
-    return pull(rootFolder, targetPath, defaultLocale, raw);
+    return update(rootFolder, targetPath, defaultLocale);
   }
   else {
     console.error('Command not found');
