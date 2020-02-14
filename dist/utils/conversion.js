94c35f91422532632480696ef1f5e03bc118183a
nicmosc <nicmosc@users.noreply.github.com>
2020-02-14 14:42:54 +0100

    new build

--- dist/utils/conversion.js
+++ dist/utils/conversion.js
@@ -6,14 +6,19 @@ Object.defineProperty(exports, "__esModule", {
 exports.jsonToYml = jsonToYml;
 exports.ymlToJson = ymlToJson;
 
-var _jsYaml = require('js-yaml');
+var _jsYaml = require('@nicmosc/js-yaml');
 
 var _jsYaml2 = _interopRequireDefault(_jsYaml);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function jsonToYml(json) {
-  return _jsYaml2.default.safeDump(json);
+function jsonToYml(json, format) {
+  return _jsYaml2.default.dump(json, {
+    sortKeys: true,
+    lineWidth: -1,
+    noCompatMode: true,
+    scalarQuoteStyle: format === undefined ? 'lokalise' : null
+  });
 }
 
 function ymlToJson(yml) {
