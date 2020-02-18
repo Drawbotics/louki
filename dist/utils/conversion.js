047ef802bb7e28aaa450213e8eb7cd6186783785
nicmosc <nicmosc@users.noreply.github.com>
2020-02-18 08:54:59 +0100

    use the same sorting algorithm as lokalise

--- dist/utils/conversion.js
+++ dist/utils/conversion.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 exports.jsonToYml = jsonToYml;
 exports.ymlToJson = ymlToJson;
 
@@ -14,11 +17,22 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var pluralForms = ['zero', 'one', 'two', 'few', 'many', 'other'];
 
-function lokaliseSort(a, b) {
+function lokaliseSort(a, b, object) {
+  var aValue = object[a];
+  var bValue = object[b];
+
   if (pluralForms.includes(a)) {
     // handle plural forms
-    console.log(a, b);
     return pluralForms.indexOf(a) - pluralForms.indexOf(b);
+  }
+  if (b.startsWith(a) && b.split('_')[0] === a && (typeof bValue !== 'string' || typeof aValue !== 'string')) {
+    // handle e.g. project vs project_item
+    console.log(a, typeof aValue === 'undefined' ? 'undefined' : _typeof(aValue), b, typeof bValue === 'undefined' ? 'undefined' : _typeof(bValue));
+    return 1;
+  }
+  if (a.startsWith(b) && a.split('_')[0] === b && (typeof bValue !== 'string' || typeof aValue !== 'string')) {
+    // handle e.g. project vs project_item
+    return -1;
   }
   return a.localeCompare(b);
 }
--- src/utils/conversion.js
+++ src/utils/conversion.js
@@ -4,10 +4,21 @@ import yaml from '@nicmosc/js-yaml';
 const pluralForms = ['zero', 'one', 'two', 'few', 'many', 'other'];
 
 
-function lokaliseSort(a, b) {
+function lokaliseSort(a, b, object) {
+  const aValue = object[a];
+  const bValue = object[b];
+
   if (pluralForms.includes(a)) {  // handle plural forms
-    console.log(a, b);
     return pluralForms.indexOf(a) - pluralForms.indexOf(b);
+  }
+  if (b.startsWith(a) && b.split('_')[0] === a
+    && (typeof bValue !== 'string' || typeof aValue !== 'string')) {  // handle e.g. project vs project_item
+    console.log(a, typeof aValue,b,  typeof bValue);
+    return 1;
+  }
+  if (a.startsWith(b) && a.split('_')[0] === b
+    && (typeof bValue !== 'string' || typeof aValue !== 'string')) {  // handle e.g. project vs project_item
+    return -1;
   }
   return a.localeCompare(b);
 }
